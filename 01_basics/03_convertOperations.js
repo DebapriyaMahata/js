d6f22dd3a982be6aa3edea7442c4a949b24881cb
Debapriya Mahata <127397786+DebapriyaMahata@users.noreply.github.com>
2025-04-09 13:59:14 +0000

    convertion of data types

---
+++ 01_basics/03_convertOperations.js
@@ -0,0 +1,19 @@
+//EX:1
+let score="33";
+let newScore=Number(score);//converting the string into number
+console.log(newScore);// 33(number)
+
+//EX:2
+let score2="33aa";
+let newScore2=Number(score2);//converting the string into number
+console.log(newScore2);//it will convert in NUMBER but output will came NaN
+
+//EX:3
+let logedIn=1;
+let logg=Boolean(logedIn)//converting num to boolean
+console.log(logg);//output->true
+//0->false
+//1->true
+// "debu"->true
+
+//EX: 4 same also for string
