bee98fc7a153a7e9d3ac182b316b97832ecddc1c
Debapriya Mahata <127397786+DebapriyaMahata@users.noreply.github.com>
2025-04-09 12:51:59 +0000

    variables in js

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+console.log("hellow world");
+
+// let,const,var
+let studentId=220468;
+let studentName="debapriya Mahata"
+const adhararNo=65012350;//never changed
+var sec="b";//now it is not used in js
+
+console.log(adhararNo);
+
+
+console.table([studentId,studentName,adhararNo,sec]);
+
+
