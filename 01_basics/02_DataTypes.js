5f5e446542c3ab103ccfd7e81c4b8fb84c16aa16
Debapriya Mahata <127397786+DebapriyaMahata@users.noreply.github.com>
2025-04-09 13:22:05 +0000

    dataTypes

---
+++ 01_basics/02_DataTypes.js
@@ -0,0 +1,24 @@
+"use strict" //treat all js code as a newer version of js
+
+//For documentation of js ->tc39,mdn
+
+//data types are-> number,string,boolean,null,undefined,bigint,symbol
+
+//number->2 to pow 53
+//null-> standalone value
+//undefined->
+//symbol->unique
+//
+
+let name="debu"//string
+let age=21;//number
+let isMarried=false;//boolean
+let job=null;//null
+let project;//undefined
+
+// typeof()-> is used to known the types of data
+
+console.log(typeof(name));
+console.log(typeof null); //ans:object
+
+
