732b2f29777b3c603f4d788a6caf2f721127c1d8
Debapriya Mahata <127397786+DebapriyaMahata@users.noreply.github.com>
2025-04-09 14:23:00 +0000

    operations in js

---
+++ 01_basics/04_operations.js
@@ -0,0 +1,30 @@
+
+let num=22;
+num=-(num);
+console.log(num);
+
+console.log(2+2);//addition
+console.log(2-1);//subtraction
+console.log(2*2);//multiplication
+console.log(2/2);//division
+console.log(2**4);//exponential
+console.log(2%3);//modulo
+
+let firstName="debu"
+let lastName="mahata"
+console.log(firstName+" "+lastName);//concatination
+
+//tricky things
+console.log("2"+1+1+1);//ans:2111
+console.log(1+1+1+"2");//ans:32
+
+console.log(+true);//ans:1
+//console.log(true+);eror
+
+
+
+
+
+
+
+
