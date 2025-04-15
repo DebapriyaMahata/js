71de6a45a0d6a9b1bf9c18cbed0df06a0558e126
Debapriya Mahata <127397786+DebapriyaMahata@users.noreply.github.com>
2025-04-15 12:36:30 +0000

    string methods in js

---
+++ 01_basics/07_String.js
@@ -0,0 +1,34 @@
+let name="Debapriya"
+let repoCount=10
+
+console.log("my name is:"+name+ " and my repo count is:"+repoCount);// not reliable method
+
+console.log(`my name is ${name} and my repo count is: ${repoCount}`);// reliable methode
+// another types to declare a string
+let game=new String("pubg")
+console.log(game[0]);
+console.log("lowerCase: "+game.toLocaleUpperCase());
+console.log("indexof: "+game.indexOf('p'));
+console.log("charat: "+game.charAt(1));
+console.log("substring: "+game.substring(0,3));
+let namee="  ankusha  "
+console.log("trim:"+namee.trim());
+console.log("namee:"+namee.replace("  ",""));
+console.log(namee.replace('a','o'));//here replace only first one not alll
+//split
+
+
+
+
+
+//slice
+console.log("slice:"+game.slice(-3,2));//'-' means from last it will decrease
+
+//Splice
+
+
+
+
+
+
+
