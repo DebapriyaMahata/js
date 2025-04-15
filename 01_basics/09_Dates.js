f994ad7ebc9c888400507d0389cbea0accb8f428
Debapriya Mahata <127397786+DebapriyaMahata@users.noreply.github.com>
2025-04-15 19:02:43 +0000

    dates in js

---
+++ 01_basics/09_Dates.js
@@ -0,0 +1,19 @@
+//Dates
+let date=new Date()
+console.log(date);//2025-04-15T14:07:51.978Z
+console.log(date.toDateString());//Tue Apr 15 2025
+console.log(date.toISOString());//2025-04-15T14:07:51.978Z
+console.log(date.toLocaleString());//4/15/2025, 4:54:33 PM
+
+let newDates=new Date(2023,3,15,10,56);
+console.log(newDates.toLocaleString())//4/15/2023, 10:56:00 AM
+
+let timeStamp=Date.now()
+console.log(timeStamp);//milisecond value will came
+
+
+
+
+
+
+
